ff895610e3269a6a80caca80ed6b4301faa2c814
Partha De <47812891+parthade92@users.noreply.github.com>
2021-07-13 21:34:39 +0530

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -5,5 +5,9 @@ cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 length = len(cards)
 card1 = cards[r.randint(0, length-1)]
 card2 = cards[r.randint(0, length-1)]
-print(f"Your cards:\n [{card1} , {card2}]")
-sum = card1 + card2
+print(f"Your cards: [{card1}, {card2}]")
+total = card1 + card2
+comp_card = cards[r.randint(0, length-1)]
+print(f"Computer's first card: {comp_card}")
+another_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
+
