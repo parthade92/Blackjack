ddb87e9daf3227b6f2ec61b8a4f022bf9973b8e2
Partha De <47812891+parthade92@users.noreply.github.com>
2021-07-14 09:39:54 +0530

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,16 @@
 import random
 import time
 
-
+logo = """
+.------.            _     _            _    _            _    
+|A_  _ |.          | |   | |          | |  (_)          | |   
+|( \/ ).-----.     | |__ | | __ _  ___| | ___  __ _  ___| | __
+| \  /|K /\  |     | '_ \| |/ _` |/ __| |/ / |/ _` |/ __| |/ /
+|  \/ | /  \ |     | |_) | | (_| | (__|   <| | (_| | (__|   < 
+`-----| \  / |     |_.__/|_|\__,_|\___|_|\_\ |\__,_|\___|_|\_\\
+      |  \/ K|                            _/ |                
+      `------'                           |__/           
+"""
 print(logo)
 # p_count = 1
 # c_count = 1
@@ -17,7 +26,7 @@ print(f"Your cards: {player}")
 player_total = sum(player)
 print(f"Your Total: {player_total}")
 if player_total == 21:
-    print("Blackjack, You win")
+    print("Blackjack, You win 🤪")
     exit(0)
 comp.append(cards[random.randint(0, length - 1)])
 comp_total = sum(comp)
@@ -25,7 +34,7 @@ print(f"Computer's first card: {comp}")
 another_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
 while another_card == 'y':
     if player_total > 21:
-        print(f"Dealer Wins.")
+        print(f"Dealer Wins. 😭")
         exit(0)
     else:
         player.append(cards[random.randint(0, length - 1)])
@@ -33,7 +42,7 @@ while another_card == 'y':
         player_total = sum(player)
         print(f"Your Total: {player_total}")
         if player_total > 22:
-            print(f"Dealer Wins.")
+            print(f"Dealer Wins.😭")
             exit(0)
         else:
             another_card = input("Type 'y' to get another card, type 'n' to pass: ").lower()
@@ -43,15 +52,15 @@ while another_card == 'n' and comp_total < 22:
     comp_total = sum(comp)
     print(f"Dealer Total: {comp_total}")
     if player_total < comp_total < 22:
-        print("Dealer Wins.")
+        print("Dealer Wins.😭")
         exit(0)
-    elif comp_total > 22:
-        print("You Win")
+    elif comp_total > 21:
+        print("You Win 🤪")
         exit(0)
     elif comp_total == player_total:
-        print("Draw.")
+        print("Draw.🙁")
         exit(0)
     elif comp_total == 21:
-        print("Dealer Wins.")
+        print("Dealer Wins.😭")
         exit(0)
     time.sleep(2)
